28588f411f00a244a0fe76959e1446bb930196e9
Qasimala <qasimala786@gmail.com>
2024-04-23 12:09:55 +0200

    added solution for https://www.codewars.com/kata/53ee5429ba190077850011d4

---
+++ 8kyu You Can't Code Under Pressure #1.dart
@@ -0,0 +1,15 @@
+// instructions:
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// starter code
+
+// int doubleInteger(int i) {
+//   return 0;
+// }
+
+// Solution
+
+int doubleInteger(int i) {
+  return i * 2;
+}
